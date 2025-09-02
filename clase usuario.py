ee21e74996e4a0dc00c29a1168fabe6a5764d735
Alejandro de León <pepedoy1213@gmail.com>
2025-09-01 22:14:23 -0600

    Actualización clase estudiante

--- clase usuario.py
+++ clase usuario.py
@@ -17,5 +17,11 @@ class Usuario:
 class Estudiante(Usuario):
     def __init__(self, id_usuario, nombre, email):
         super().__init__(id_usuario, nombre, email)
-        self.cursos_inscritos = []  
-        self.calificaciones = {}   
+        self.cursos_inscritos = []  # Lista para lso cursos inscritos
+        self.calificaciones = {}   #voy a usar un diccionario para q se guarden las calificaciones
+
+    def inscribir_curso(self, curso):               #Un error, para ver si el estudiante ya esta inscrito
+        if curso in self.cursos_inscritos:
+            raise ValueError(f"El estudiante {self.nombre}, ya está inscrito en este curso")
+        self.cursos_inscritos.append(curso)
+        print(f"Estudiante: {self.nombre}, ha sido inscrito en el curso: {curso.nombre}")
