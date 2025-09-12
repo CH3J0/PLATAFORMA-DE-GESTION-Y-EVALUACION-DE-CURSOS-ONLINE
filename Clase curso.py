d2882af9e83d9585f5448b2a87b9d3707c6e9c7a
Alejandro de León <pepedoy1213@gmail.com>
2025-09-12 17:17:20 -0600

    Validaciones generales

--- Clase curso.py
+++ Clase curso.py
@@ -47,3 +47,32 @@ class Evaluacion:
 
     def __str__(self):
         return f"{self.nombre} ({self.tipo})"
+
+#validaciones
+def pedir_codigo():
+    while True:
+        codigo = input("Ingrese el código del curso (solo números): ")
+        if codigo.isdigit():
+            return int(codigo)
+        print("Error: El código debe contener solo números.")
+
+def pedir_nombre_curso():
+    while True:
+        nombre = input("Ingrese el nombre del curso (sin números): ")
+        if nombre.replace(" ", "").isalpha():
+            return nombre
+        print("Error: El nombre no debe contener números ni símbolos.")
+
+def pedir_nombre_evaluacion():
+    while True:
+        nombre = input("Ingrese el nombre de la evaluación: ")
+        if nombre.strip():
+            return nombre
+        print("Error: El nombre no puede estar vacío.")
+
+def pedir_tipo_evaluacion():
+    while True:
+        tipo = input("Ingrese el tipo de evaluación (Ej: Examen, Proyecto, Tarea): ")
+        if tipo.replace(" ", "").isalpha():
+            return tipo
+        print("Error: El tipo debe contener solo letras.")
