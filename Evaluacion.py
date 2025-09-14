8b25e0ddea5e8ba7e2ff61fb457f235eaf06a7fc
34carlos.ch <34carlosa.ch@gmail.com>
2025-09-14 00:06:07 -0600

    Agrego subclase Examen heredando de Evaluacion

--- Evaluacion.py
+++ Evaluacion.py
@@ -14,3 +14,11 @@ class Evaluacion:
 
     def __str__(self):
         return f"Evaluación: {self.nombre}"
+
+class Examen(Evaluacion):
+    def __init__(self, nombre, curso, duracion):
+        super().__init__(nombre, curso)
+        self.duracion = duracion
+
+    def __str__(self):
+        return f"Examen: {self.nombre}, Duración: {self.duracion} min"
