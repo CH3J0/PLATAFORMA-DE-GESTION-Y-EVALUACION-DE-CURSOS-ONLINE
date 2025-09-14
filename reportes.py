9ec835449f4a32a7b107d04a8ae727bc49d7e9bf
34carlos.ch <34carlosa.ch@gmail.com>
2025-09-14 01:24:30 -0600

    agregar tipo de evaluación al listado de notas

--- reportes.py
+++ reportes.py
@@ -1,3 +1,13 @@
+def tipo_de_evaluacion(evaluacion):
+    if isinstance(evaluacion, Examen):
+        return "Examen"
+    elif isinstance(evaluacion, Tarea):
+        return "Tarea"
+    elif isinstance(evaluacion, Proyecto):
+        return "Proyecto"
+    else:
+        return "Evaluacion"
+    
 def reporte_promedio_bajo(curso, umbral=60):
     """
     Genera un reporte de estudiantes con promedio bajo en un curso.
@@ -5,15 +15,15 @@ def reporte_promedio_bajo(curso, umbral=60):
     print(f"\n--- REPORTE: Estudiantes con promedio menor a {umbral} ---")
     for estudiante in curso.estudiantes_inscritos:
         notas = [
-            (eval.nombre, eval.notas[estudiante])
+            (eval.nombre, tipo_de_evaluacion(eval), eval.notas[estudiante])
             for eval in curso.evaluaciones
             if estudiante in eval.notas
         ]
         if notas:
-            promedio = sum(n[1] for n in notas) / len(notas)
+            promedio = sum(n[2] for n in notas) / len(notas)
             if promedio < umbral:
                 print(f"\nEstudiante: {estudiante.nombre} | ID: {estudiante.id_usuario} | Email: {estudiante.email}")
                 print(f"Promedio: {promedio:.2f}")
                 print("Notas por evaluación:")
-                for nombre_eval, nota in notas:
-                    print(f"  - {nombre_eval}: {nota}")
+                for nombre_eval,tipo, nota in notas:
+                    print(f"  - {nombre_eval} ({tipo}): {nota}")
